89368087841d7df309dd1343cc04a8159226ee71
zelatndnjs <zelatndnjs@naver.com>
2025-02-04 23:03:50 +0900

    5.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -26,7 +27,7 @@ class TikTokApp extends StatelessWidget {
               fontWeight: FontWeight.w600),
         ),
       ),
-      home: SignUpScreen(),
+      home: InterestsScreen(),
     );
   }
 }
